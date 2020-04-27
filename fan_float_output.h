5894d43079fddf259947680ffed02302e48897e9
fancygaphtrn <fancygaphtrn@gmail.com>
2020-04-27 08:02:29 -0400

    Add files via upload

---
+++ fan_float_output.h
@@ -0,0 +1,65 @@
+#include "esphome.h"
+using namespace esphome;
+
+class MyCustomFloatOutput : public Component, public FloatOutput {
+ public:
+  void setup() override {
+    // This will be called by App.setup()
+    pinMode(D1, OUTPUT);
+    pinMode(D5, OUTPUT);
+    pinMode(D6, OUTPUT);
+    pinMode(D7, OUTPUT);
+  }
+
+  void write_state(float state) override {
+    // state 1 = high
+	// state 0.66 = medium
+	// state 0.33 = low
+	// state 0 = off
+    digitalWrite(D1, LOW);
+    digitalWrite(D5, LOW);
+    digitalWrite(D6, LOW);
+    digitalWrite(D7, LOW);
+    
+    if ( state == 1 ) {
+      ESP_LOGD("custom", "High");
+      digitalWrite(D5, HIGH);
+	  delay(1000);
+      digitalWrite(D5, LOW);
+	  delay(500);
+      digitalWrite(D5, HIGH);
+	  delay(1000);
+      digitalWrite(D5, LOW);
+    }
+    if ( state == 0.66f ) {
+      ESP_LOGD("custom", "Medium");
+      digitalWrite(D6, HIGH);
+	  delay(1000);
+      digitalWrite(D6, LOW);
+	  delay(500);
+      digitalWrite(D6, HIGH);
+	  delay(1000);
+      digitalWrite(D6, LOW);
+    }
+    if ( state == 0.33f ) {
+      ESP_LOGD("custom", "Low");
+      digitalWrite(D7, HIGH);
+	  delay(1000);
+      digitalWrite(D7, LOW);
+	  delay(500);
+      digitalWrite(D7, HIGH);
+	  delay(1000);
+      digitalWrite(D7, LOW);
+    }
+    if ( state == 0 ) {
+     ESP_LOGD("custom", "Off");
+      digitalWrite(D1, HIGH);
+	  delay(1000);
+      digitalWrite(D1, LOW);
+	  delay(500);
+      digitalWrite(D1, HIGH);
+	  delay(1000);
+      digitalWrite(D1, LOW);
+    }
+  }
+};
