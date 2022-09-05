fee3847e507858486c306fc688a1733548a99d1a
fancygaphtrn <fancygaphtrn@gmail.com>
2022-09-05 08:06:33 -0400

    Add files via upload

--- fan_float_output.h
+++ fan_float_output.h
@@ -16,6 +16,9 @@ class MyCustomFloatOutput : public Component, public FloatOutput {
 	// state 0.66 = medium
 	// state 0.33 = low
 	// state 0 = off
+
+    ESP_LOGD("custom", "State changed to %f", state);
+
     digitalWrite(D1, LOW);
     digitalWrite(D5, LOW);
     digitalWrite(D6, LOW);
@@ -24,41 +27,41 @@ class MyCustomFloatOutput : public Component, public FloatOutput {
     if ( state == 1 ) {
       ESP_LOGD("custom", "High");
       digitalWrite(D5, HIGH);
-	  delay(1000);
+      delay(1000);
       digitalWrite(D5, LOW);
-	  delay(500);
+      delay(500);
       digitalWrite(D5, HIGH);
-	  delay(1000);
+      delay(1000);
       digitalWrite(D5, LOW);
     }
-    if ( state == 0.66f ) {
+    if ( state > 0.6f && state < 0.7f ) {
       ESP_LOGD("custom", "Medium");
       digitalWrite(D6, HIGH);
-	  delay(1000);
+      delay(1000);
       digitalWrite(D6, LOW);
-	  delay(500);
+      delay(500);
       digitalWrite(D6, HIGH);
-	  delay(1000);
+      delay(1000);
       digitalWrite(D6, LOW);
     }
-    if ( state == 0.33f ) {
+    if ( state > 0.3f && state < 0.4f ) {
       ESP_LOGD("custom", "Low");
       digitalWrite(D7, HIGH);
-	  delay(1000);
+      delay(1000);
       digitalWrite(D7, LOW);
-	  delay(500);
+      delay(500);
       digitalWrite(D7, HIGH);
-	  delay(1000);
+      delay(1000);
       digitalWrite(D7, LOW);
     }
     if ( state == 0 ) {
      ESP_LOGD("custom", "Off");
       digitalWrite(D1, HIGH);
-	  delay(1000);
+      delay(1000);
       digitalWrite(D1, LOW);
-	  delay(500);
+      delay(500);
       digitalWrite(D1, HIGH);
-	  delay(1000);
+      delay(1000);
       digitalWrite(D1, LOW);
     }
   }
